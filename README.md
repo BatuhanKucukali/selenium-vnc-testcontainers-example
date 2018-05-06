dccef4622e89f75d465a9fb2e4bc1351c126d82b
Batuhan Küçükali <mail@batuhankucukali.com>
2018-05-06 12:52:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Selenium Test with Testcontainers Example #
+
+## Prerequisites ##
+* [Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+* [JDK](https://www.java.com/en/download/)
+* [Gradle](https://gradle.org/)
+* [Docker](https://www.docker.com/)
+
+## Run this project ##
+1 . Clone project on your machine
+```
+git clone https://github.com/BatuhanKucukali/selenium-vnc-testcontainers-example
+```
+2 . Change directory
+```
+cd selenium-vnc-testcontainers-example
+```
+3 . Run Tests
+```
+gradle clean test --info
+```
+
+## Getting help ##
+
+If you're having trouble getting this project running, feel free to [open an issue](https://github.com/BatuhanKucukali/selenium-vnc-testcontainers-example/issues/new) or [email me](mailto:mail@batuhankucukali.com)!
---
+++ src/test/java/TestSelenium.java
@@ -0,0 +1,37 @@
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+import java.io.File;
+
+import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
+
+public class TestSelenium {
+
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome())
+            .withRecordingMode(RECORD_ALL, new File("out"));
+
+    @Test
+    public void searchTitleTest() {
+        RemoteWebDriver driver = chrome.getWebDriver();
+
+        driver.get("http://www.google.com");
+
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("bumin");
+
+        element.submit();
+
+        Assert.assertTrue(driver.getTitle().contains("bumin"));
+
+        driver.quit();
+    }
+}
